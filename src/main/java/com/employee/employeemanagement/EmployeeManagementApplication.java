838d31c848cb80bd924c82fbb41f499114719ed5
zia.haider <86730092+zianhaidernaqvi@users.noreply.github.com>
2023-01-10 18:45:13 +0530

    minor change in structure

--- src/main/java/com/employee/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/employee/employeemanagement/EmployeeManagementApplication.java
@@ -2,9 +2,16 @@ package com.employee.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeManagementApplication {
+public class EmployeeManagementApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeManagementApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
